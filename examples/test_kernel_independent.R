19f58cb3c6ecd6ff0f152bc5afaa5edfe3ddce29
linqiaozhi <george.linderman@gmail.com>
2018-10-27 09:42:51 -0400

    Timing code, added example, and demo of timing cauchy_2d()

---
+++ examples/test_kernel_independent.R
@@ -0,0 +1,11 @@
+# Note: this script should be run after setting the FIt-SNE directory as the current one (e.g. setwd())
+
+source('fast_tsne.R')
+
+require(MASS);
+N <- 1E4;
+d <- 3;
+input_data <- rbind(mvrnorm(n = N/2, rep(0, d), diag(d)),
+	                  mvrnorm(n = N/2, rep(100, d), diag(d)))
+Y2 <- fftRtsne(input_data, 2, max_iter = 400, start_late_exag_iter=300, late_exag_coeff=10);
+plot(Y2[,1],Y2[,2],col=c(rep(1,N/2), rep(2,N/2))) # Plot the result
